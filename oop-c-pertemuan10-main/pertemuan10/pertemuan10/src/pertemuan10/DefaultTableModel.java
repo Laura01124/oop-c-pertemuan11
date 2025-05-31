d2d2e3b2d7969273431d46c3dd50d6ffe35d0b87
MARGARETA BUNGA KELORE <margaretabungakelore@gmail.com>
2025-05-31 14:51:00 +0800

    Add files via upload

---
+++ oop-c-pertemuan10-main/pertemuan10/pertemuan10/src/pertemuan10/DefaultTableModel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan10;
+
+/**
+ *
+ * @author LAB F
+ */
+class DefaultTableModel {
+
+    void addColumn(String nim) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addRow(Object[] dtmhs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
